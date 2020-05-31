e76c558a21c03814d5b0969f28423b6941a7b8f3
alan <alan@msi.loc>
2020-05-31 12:57:22 -0500

    initial commit

---
+++ test.py
@@ -0,0 +1,46 @@
+# pycharm
+# comment -> ctrl + /
+# alt f12 to open terminal
+
+# python3 <python file> in terminal
+# 2 ways to run
+# - terminal
+# - pycharm
+print("Hello World")
+
+# print different messages
+# print("austin")
+# print("dallas")
+# print("houston")
+
+# variables
+# string: "hello there"
+# int: 43
+# float: 2.3
+# char: 'a'
+# x = 7
+# y = 6
+# z = x + y
+# print(z)  # should be 13
+# print(x - y + 4)  # should be 5
+
+
+# input from keyboard
+# celcius to farenheit
+F = float(input("Enter farenheit degrees"))
+C = (F - 32) * (5 / 9)
+print(C)
+
+# integer input
+num = int(input("enter a number between 1 and 100"))
+print(num)
+
+# string input
+name = input("enter your name")
+print(name)
+
+# assignment 0
+# write a basic calculator w/ these functions
+# add, subtract, multiply, divide
+# take 2 numbers from user input
+# display the results on the console
