ddedde5025d21a73d59a22e557d09e24ca769f6a
alan <alan@msi.loc>
2020-06-13 21:32:45 -0500

    assignment 2 now live

---
+++ assignment2/challenges.py
@@ -0,0 +1,102 @@
+##
+# INSTRUCTIONS:
+# 1. DO NOT rename the function names or change the parameters
+# 2. Feel free to write helper functions in the HELPER FUNCTIONS area
+# 3. DO NOT write new classes to help solve the problems
+# 4. Make use of 'sandbox.py' to test/debug your code
+# 5. I will test your code using my user generated files
+# 6. Make use of the group chat & ask questions in the group chat
+#
+#
+#
+
+# HELPER FUNCTIONS
+
+##
+# write a function that adds the numbers between x and y inclusive
+# return the sum of those numbers
+# #
+def part0(x, y):
+    return 0
+
+
+##
+# write a function that checks if a  number is a perfect even
+# this means that the number has no odd factors except 1
+# returns True/False
+def part1(num):
+    return False
+
+
+##
+# convert a number to a roman numeral string
+# the number should be in range [0, 1000]
+#
+# returns uppercase string roman numeral string representation of num
+# https://www.britannica.com/topic/Roman-numeral
+def part2(num):
+    return "string"
+
+
+##
+# Write a T9 Encryption method that takes a string parameter
+# and converts it to a numerical value using a phone keypad as a cypher
+# @par
+# abc -> 2
+# def -> 3
+# ghi -> 4
+# jkl -> 5
+# mno -> 6
+# pqrs -> 7
+# tuv -> 8
+# wxyz -> 9
+# <space> ->0
+# needs to return a encoded string
+# Ex: part3("alan") = 2526
+def part3(original):
+    return "string"
+
+
+##
+# Check if 2 texts are isomorphic (each letter maps to another)
+# ex: paper and title are isomorphic
+# p -> t
+# a -> i
+# p -> t
+# e -> l
+# r -> e
+# returns True/False
+def part4(text1, text2):
+    return False
+
+
+##
+# check if a string is a palindrome: (spelt the same both ways)
+# ignore the case
+# part5(car): False
+# part5(bob): True
+# returns True/False
+#
+# ##
+def part5(text):
+    return False
+
+
+##
+# write the combination formula
+# c(n, k) = n! / ((n-k)! * k!)
+# returns the combination of 2 number
+# needs to work for large numbers and cannot timeout
+# ##
+def part6(n, k):
+    return 0
+
+
+##
+# write the permutation formula
+# c(n, k) = n! / ((n-k)! * k!)
+# returns the permutation of 2 number
+# needs to work for large numbers and cannot timeout
+# ##
+def part7(n, k):
+    return 0
---
+++ assignment2/sandbox.py
@@ -0,0 +1,8 @@
+from assignment2.challenges import *
+
+
+def main():
+    print("DO NOT delete the import statement")
+
+
+main()
